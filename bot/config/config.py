5b2cdac49e11b66b74e4700e508f1dec7ba9655a
Khondoker X Hasan <108849109+khondokerXhasan@users.noreply.github.com>
2024-12-08 15:43:46 +0600

    [TEST] Update config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -61,6 +61,6 @@ class Settings(BaseSettings):
     ADVANCED_ANTI_DETECTION: bool = False
     ENABLE_SSL: bool = True
     USE_PROXY_FROM_FILE: bool = False
-    GIT_UPDATE_CHECKER: bool = True
+    GIT_UPDATE_CHECKER: bool = False
 
 settings = Settings()
